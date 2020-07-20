26c3e988dd587232ec9f0c6773a1e22ca299ede4
sparklerfish <julie.ellen.adams@gmail.com>
2020-07-20 10:38:01 -0700

    Fix some formatting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require("./secret.json");
+const config = require(''./secret.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -8,15 +8,15 @@ client.on('ready', () => {
 
 const isGonk = (message, i = 0) => {
     const gonk = 'gonk';
-    for (let j = 0; i < gonk.length && j < message.length; j++)
+    for (let j = 0; i < gonk.length && j < message.length; j++) {
         if (gonk[i] === message[j]) i++;
+    }
     return i === gonk.length;
 };
 
 client.on('message', (message) => {
     const formattedMessage = message.content.toLowerCase();
-    if (isGonk(formattedMessage)) {
-        if (message.author.id === client.user.id) return;
+    if (isGonk(formattedMessage) && message.author.id !== client.user.id) {
         message.channel.send('GONK!');
     }
 });
