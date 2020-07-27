490fdda0ae164574a55dd19e98665f809de4ec04
sparklerfish <julie.ellen.adams@gmail.com>
2020-07-27 09:16:12 -0700

    Check for URLs and ignore them

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ client.on('ready', () => {
 const isGonk = (message, i = 0) => {
     const gonk = 'gonk';
     for (let j = 0; i < gonk.length && j < message.length; j++) {
+        if (message[j] === ":" && message[j + 1] === "/" && message[j + 2] === "/") {
+            while (message[j] !== " ") continue;
+        }
+
         if (gonk[i] === message[j]) i++;
     }
     return i === gonk.length;
 };
 
 client.on('message', (message) => {
+
     const formattedMessage = message.content.toLowerCase();
     if (isGonk(formattedMessage) && message.author.id !== client.user.id) {
         message.channel.send('GONK!');
